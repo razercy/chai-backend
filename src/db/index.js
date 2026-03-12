605f214739ea375da470d1d022d501a5b18df887
Rajarshi Chakraborty <24je0680@iitism.ac.in>
2026-03-12 16:46:25 +0530

    utils code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
+import dns from "node:dns";
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+dns.setServers(["8.8.8.8","1.1.1.1"]);
 
 const connectDB = async () => {
     try {
